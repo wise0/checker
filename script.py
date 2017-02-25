892f4bd2c6185f7f603ad60efb08bbdfbf88dbd5
wise0 <wisevertix@gmail.com>
2017-02-24 16:21:23 -0800

    Create script.py

---
+++ script.py
@@ -0,0 +1,11 @@
+import requests
+import time
+
+urls = [ # this is a list of urls that you want to ping. Feel free to add more than one.
+  "http://<your-app-name>.herokuapp.com",
+]
+
+while True:
+  for url in urls:
+    requests.get(url) # pinging the server
+  time.sleep(300) # delay for 5 minutes  
