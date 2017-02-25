b3626670e1b5ea6c70ed5e78b79550547fe241f6
wise0 <wisevertix@gmail.com>
2017-02-24 16:22:04 -0800

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 urls = [ # this is a list of urls that you want to ping. Feel free to add more than one.
-  "http://<your-app-name>.herokuapp.com",
+  "http://thecosmopolitan.herokuapp.com/",
 ]
 
 while True:
